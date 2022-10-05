7be2c838d12f2c27e759f5674766f0be3671e4e2
Didier Banlock <didier.banlock@gmail.com>
2022-10-05 17:07:51 +0200

    overrides removed

--- app/overrides/admin/shared.rb
+++
@@ -1,5 +0,0 @@
-Deface::Override.new(
-    virtual_path: 'spree/admin/shared/sub_menu/_product',
-    name: "add_qa_tab",
-    insert_bottom: "[data-hook='admin_product_sub_tabs']",
-    text: "<%= tab :qna, url: pending_admin_product_questions_path, label: t('pending_questions'), match_path: '/questions/pending' %>")
